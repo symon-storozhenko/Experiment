95c61e3cf0c6f587b569100dc0547c45b748fa2f
symonstorozhenko <stosimon@gmail.com>
2021-08-03 20:14:48 -0400

    refactored cli_parser.py to add context manager and stderr to error.log

--- cli_parser.py
+++ cli_parser.py
@@ -1,15 +1,27 @@
 import sys
 import csv
+from csv import DictReader, DictWriter
+import datetime
+from pathlib import Path
 
+list_of_dict = []
 try:
-    if sys.argv[1] == '-':
-        f = sys.stdin.read().splitlines()
-    else:
-        filename = sys.argv[1]
-        f = open(filename, 'r')
-    csv = csv.reader(f)
-    data = list(csv)
-    for row in data:
-        print(row)
-except Exception as e:
-    print("Error Reading from file:")
+    filename = sys.argv[1]
+    with open(filename, mode='r', encoding='utf-8-sig') as read_obj:
+        dict_reader = DictReader(read_obj)
+        list_of_dict = list(dict_reader)
+except FileNotFoundError as e:
+    print(f"{datetime.datetime.now()} - {filename} file not found")
+
+
+# Iterate over the dicts
+for i in list_of_dict:
+    print(f'with function -> {i}')
+    if i['type'] == 'deposit':
+        print('deposit!! YAy!')
+
+# output = sys.argv[3]
+path = Path.home() / "testo.csv"
+with open(path, mode='w', encoding='utf-8-sig') as cust_accts:
+    # d = DictWriter(cust_accts)
+    pass
